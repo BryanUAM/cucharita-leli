dbcda0b060aa0a1474b6be29d3277a65863a4d09
BryanUAM <bryan.castro5@uamcr.net>
2026-03-18 09:08:23 -0600

    Crear tablas SQLite al iniciar

--- CucharitaLeliQR/Program.cs
+++ CucharitaLeliQR/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -12,25 +11,27 @@ builder.Services.AddDbContext<SodaContext>(options =>
 
 var app = builder.Build();
 
+// Crear base de datos y tablas automáticamente
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SodaContext>();
+    db.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-app.UseStaticFiles(); // 🔥 MOVER AQUÍ (ANTES)
-
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.Run();
