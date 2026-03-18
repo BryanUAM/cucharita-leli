17ed105ffd927d142eb5379e407a6773a4a6c946
BryanUAM <bryan.castro5@uamcr.net>
2026-03-18 08:24:04 -0600

    Cambiar base de datos a SQLite

--- CucharitaLeliQR/Program.cs
+++ CucharitaLeliQR/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<SodaContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
